743e66e0c2f9db55c2c5b04de17fb4f3ed966d22
Tobias Deekens <nerd@tdeekens.name>
2013-11-09 13:35:16 +0100

    Adds JsonHandler.

--- lib/Scandio/lmvc/modules/rendering/handlers/JsonHandler.php
+++ lib/Scandio/lmvc/modules/rendering/handlers/JsonHandler.php
@@ -6,6 +6,28 @@ class Json extends AbstractHandler
 {
     public function render($template = null)
     {
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires: Mon, 26 Jul 1964 07:00:00 GMT');
 
+        $json = $this->_buildJson();
+
+        if (isset($_GET['callback']) && !empty($_GET['callback'])) {
+            header('Content-type: application/javascript');
+
+            $callback = $_GET['callback'];
+
+            $json = $callback . '( return ' . $json . ');';
+        } else {
+            header('Content-type: application/json');
+        }
+
+        echo $json;
+
+        return true;
+    }
+
+    private function _buildJson()
+    {
+        return json_encode($this->getRenderArgs());
     }
 }
