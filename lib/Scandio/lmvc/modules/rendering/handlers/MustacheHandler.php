38f7d1d07782016cfe8e09c9973cabd859bff01e
Tobias Deekens <nerd@tdeekens.name>
2013-11-09 13:23:08 +0100

    Adds MustacheHandler and updates AbstractHandler.

--- lib/Scandio/lmvc/modules/rendering/handlers/MustacheHandler.php
+++ lib/Scandio/lmvc/modules/rendering/handlers/MustacheHandler.php
@@ -4,8 +4,21 @@ namespace Scandio\lmvc\modules\rendering\handlers;
 
 class MustacheHandler extends AbstractHandler
 {
+    private
+        $_mustacheEngine = null;
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->_mustacheEngine = new \Mustache_Engine();
+    }
+
     public function render($template)
     {
-
+        return $this->_mustacheEngine->render(
+            $this->getFile($template),
+            $data
+        );
     }
 }
