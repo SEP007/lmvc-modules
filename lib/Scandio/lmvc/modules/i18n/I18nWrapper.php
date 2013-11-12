1f4d9858dc04a7bd5b5471056e281d9ffb82d46a
konstantinos-chronis <konstantinos.chronis@gmail.com>
2013-11-12 04:23:21 -0500

    Add JSON support for locale files

--- lib/Scandio/lmvc/modules/i18n/I18nWrapper.php
+++ lib/Scandio/lmvc/modules/i18n/I18nWrapper.php
@@ -5,6 +5,7 @@
     use Scandio\lmvc\modules\session\Session;
     use Scandio\lmvc\LVCConfig;
     use Scandio\lmvc\modules\i18n\handler\YamlHandler;
+    use Scandio\lmvc\modules\i18n\handler\JsonHandler;
     
     class I18nWrapper {
         
@@ -17,25 +18,37 @@
         public static function loadIfNeeded($rootDirectory) {        
             static::$language = Session::get('i18n.language', LVCConfig::get()->I18n->default);
        
-            /*if the language is not stored this means we have used
+            /*if the language is stored this means we have used
              *it in the past and thus the file is already loaded.
              */
-            if(!self::storeSelectedLanguage(static::$language))
-                return;
+           if(self::isSelectedLanguageStored(static::$language)) {
+               return;
+           }
             
-            $filePath = $rootDirectory . 
+            $path = $rootDirectory . 
                             DIRECTORY_SEPARATOR . LVCConfig::get()->I18n->path . 
-                            DIRECTORY_SEPARATOR . static::$language . '.yaml';
+                            DIRECTORY_SEPARATOR . static::$language;
                             
-            self::loadFile($filePath);
+            self::loadFile($path);
          }
             
-        private static function loadFile($filePath) {
+        private static function loadFile($path) {
+            
+            $format = LVCConfig::get()->I18n->format;
+            
+            $filePath = $path . '.' . $format;
+            
+            if($format == 'yaml') {
+                self::$translations = YamlHandler::loadFile($filePath);
+            }     
+            elseif ($format == 'json') {
+                self::$translations = JsonHandler::loadFile($filePath);
+            }        
+            
             if(!file_exists($filePath)) {
                  // write in the logger
             }
-    
-            self::$translations = YamlHandler::loadFile($filePath);
+           
         }
         
         public static function translate($key) 
@@ -53,18 +66,18 @@
             return Session::get('i18n.language', static::$language);
         }
         
-        private static function storeSelectedLanguage($language) 
+        private static function isSelectedLanguageStored($language) 
         {
             static::$selectedLanguages = Session::get('i18n.selectedLanguages', []);
             
-            if(!in_array($language, static::$selectedLanguages)) {
-               array_push(static::$selectedLanguages, $language);
-               Session::set('i18n.selectedLanguages', static::$selectedLanguages); 
-               
-               return true;
+            if(in_array($language, static::$selectedLanguages)) {
+               return false;
             }
             
-            return false;
+            array_push(static::$selectedLanguages, $language);
+            Session::set('i18n.selectedLanguages', static::$selectedLanguages); 
+               
+            return true;
         }
         
     }
